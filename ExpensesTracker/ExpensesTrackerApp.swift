558e6e3065880ec32ce921a3f732f9bbdc3b1e64
lsotofbdev <108716943+lsotofbdev@users.noreply.github.com>
2025-01-17 02:33:37 -0500

    Initial Commit

---
+++ ExpensesTracker/ExpensesTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerApp.swift
+//  ExpensesTracker
+//
+//  Created by Luis Angel Soto Flores on 17/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpensesTrackerTests/ExpensesTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesTrackerTests.swift
+//  ExpensesTrackerTests
+//
+//  Created by Luis Angel Soto Flores on 17/01/25.
+//
+
+import Testing
+@testable import ExpensesTracker
+
+struct ExpensesTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
